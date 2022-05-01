cbae481eb28c47845f5c03678e5649f9f4c8f6e4
Reginaldo Silva <resilvajr@hotmail.com>
2022-05-01 11:42:08 -0300

    criada uma api para listar categorias

---
+++ produtos/categorias.php
@@ -0,0 +1,19 @@
+<?php
+
+include_once("../conexao.php");
+
+$dados = array();
+
+$query = $pdo->query("SELECT * from categorias order by produtos desc");
+$res = $query->fetchAll(PDO::FETCH_ASSOC);
+
+for ($i=0; $i < count($res); $i++){
+    foreach ($res[$i] as $key => $value){}
+        $dados = $res;
+}
+
+echo ($res) ?
+json_encode(array("code" => 1, "result"=>$dados)) :
+json_encode(array("code" => 0, "result"=>"Dados não encontrados!"))
+
+?>
