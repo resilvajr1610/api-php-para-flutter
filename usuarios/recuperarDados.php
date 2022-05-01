9b6eb7f5dd6f5d5a6e9e01e608eb7a94a2436e77
Reginaldo Silva <resilvajr@hotmail.com>
2022-05-01 00:24:08 -0300

    criada uma api para recuperar os dados dos usuários cadastrados

---
+++ usuarios/recuperarDados.php
@@ -0,0 +1,23 @@
+<?php
+
+include_once("../conexao.php");
+
+   $id = .$_GET['id'];
+
+$dados = array();
+
+$query = $pdo->query("SELECT * from usuarios where id like '$id'");
+$res = $query->fetchAll(PDO::FETCH_ASSOC);
+
+for ($i=0; $i < count($res); $i++){
+    foreach ($res[$i] as $key => $value){}
+
+        $dados = $res;
+
+}
+
+echo ($res) ?
+json_encode(array("code" => 1, "result"=>$dados)) :
+json_encode(array("code" => 0, "result"=>"Dados não encontrados!"))
+
+?>
